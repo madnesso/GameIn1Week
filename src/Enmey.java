d52b529f2c01886e981be0982385644648a89634
hp <deadsasuke@gmail.com>
2019-05-29 03:51:06 +0200

    Cleaning Up

---
+++ src/Enmey.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+//so far mafish fe dma8y 8er wa7d like a zombie horde cloae range walk slowly law lamess 7ad by3ml dmg ay 7ad tany 3ando afkar ?
+public class Enmey extends GameObject
+    {
+    public Enmey(float x, float y, ID id)
+        {
+        super(x, y, id);
+        }
+
+    @Override
+    public void tick()
+        {
+
+        }
+
+    @Override
+    public void paint(Graphics g)
+        {
+
+        }
+    }
