2b8488585a130ee3a676493ca2de3a9425a53370
hamaada29 <50968276+hamaada29@users.noreply.github.com>
2019-05-31 11:58:03 +0200

    Update Healer.java

--- src/Healer.java
+++ src/Healer.java
@@ -1,33 +1,85 @@
-import java.awt.*;
-
-public class Healer extends Units
-    {
-    private int mana;
-    private boolean hasmana = false;
-
-    public Healer(ID id, Point point, Dimension dimension)
-        {
-        super(id, point, dimension);
-        }
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
 
 
-    public int getMana()
-        {
+public class Healer extends Units implements IClamp{
+    
+    private float heal;
+    private float mana;
+    private boolean hasMana;
+    private float range;
+    
+    public Healer(Point point, Dimension dimention, ID id, int x, int y) {
+        super(point, dimention, id);
+        this.setVelX(x);
+        this.setVelY(y);
+    }
+
+    public float getHeal() {
+        return heal;
+    }
+
+    public void setHeal(float heal) {
+        this.heal = clamp(heal,0,100);
+    }
+
+    public float getMana() {
         return mana;
-        }
+    }
 
-    public void setMana(int mana)
-        {
-        this.mana = mana;
-        }
+    public void setMana(float mana) {
+        this.mana = clamp(mana,0,100);
+    }
 
-    public boolean isHasmana()
-        {
-        return hasmana;
-        }
+    public boolean isHasMana() {
+        return hasMana;
+    }
 
-    public void setHasmana(boolean hasmana)
+    public void setHasMana(boolean hasMana) {
+        this.hasMana = hasMana;
+    }
+
+    public float getRange() {
+        return range;
+    }
+
+    public void setRange(float range) {
+        this.range = clamp(range,0,100);
+    }
+
+    @Override
+    public void tick() {
+        this.getPoint().translate(this.getVelX(), this.getVelY());
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public float Heal(float allyRange){
+        if(this.range > allyRange)
         {
-        this.hasmana = hasmana;
+            if(this.mana>0)
+            {
+            return this.heal;
+            }
+            return 0;
+        }
+        else 
+        {
+            return 0;
         }
     }
+
+    @Override
+    public float clamp(float var, float min, float max) {
+        if(var >= max)
+            return var = max;
+        else if(var <= min)
+            return var = min;
+        else 
+            return var;
+    }
+}
