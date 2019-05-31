319e7686cd8bd9575b639b86018808ac4538c25f
hamaada29 <50968276+hamaada29@users.noreply.github.com>
2019-05-31 12:02:20 +0200

    Update Units.java

--- src/Units.java
+++ src/Units.java
@@ -1,69 +1,59 @@
-import java.awt.*;
 
-public class Units extends GameObject
-    {
-    private int Health;
-    private int AttackDmg;
-    private int Range;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+
+
+public abstract class Units extends GameObject {
+    
+    private float Health;
+    private float AttackDmg;
     private boolean AttackCd;
-    private Handler handler;
 
-    public Units(ID id, Point point, Dimension dimension)
-        {
-        super(id, point, dimension);
-        }
-
-
-    public int getHealth()
-        {
-        return Health;
-        }
-
-    public void setHealth(int health)
-        {
-        Health = health;
-        }
-
-    public int getAttackDmg()
-        {
-        return AttackDmg;
-        }
-
-    public void setAttackDmg(int attackDmg)
-        {
-        AttackDmg = attackDmg;
-        }
-
-    public int getRange()
-        {
-        return Range;
-        }
-
-    public void setRange(int range)
-        {
-        Range = range;
-        }
-
-    public boolean isAttackcd()
-        {
-        return AttackCd;
-        }
-
-    public void setAttackcd(boolean attackCd)
-        {
-        AttackCd = attackCd;
-        }
-
-    @Override
-    public void tick()
-        {
-
-        }
-
-    @Override
-    public void paint(Graphics g)
-        {
-
-        }
-
+    public Units(Point point, Dimension dimension, ID id) {
+        super(point, dimension, id);
     }
+
+    public float getHealth() {
+        return Health;
+    }
+
+    public void setHealth(float Health) {
+        if(Health >= 500)
+            this.Health = 500;
+        else if(Health <= 0)
+            this.Health = 0;
+        else 
+            this.Health = Health;
+    }
+
+    public float getAttackDmg() {
+        return AttackDmg;
+    }
+
+    public void setAttackDmg(float AttackDmg) {
+        if(AttackDmg >= 500)
+            this.AttackDmg = 500;
+        else if(AttackDmg <= 0)
+            this.AttackDmg = 0;
+        else 
+            this.Health = AttackDmg;
+    }
+
+    public boolean isAttackCd() {
+        return AttackCd;
+    }
+
+    public void setAttackCd(boolean AttackCd) {
+        this.AttackCd = AttackCd;
+    }
+
+    @Override
+    public void tick() {
+        this.getPoint().translate(this.getVelX(), this.getVelY());
+    }
+
+    @Override
+    public abstract void paint(Graphics g);
+    
+}
