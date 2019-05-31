40aa113b81c1bdcc02d387e476e5981792c9d6ff
hamaada29 <50968276+hamaada29@users.noreply.github.com>
2019-05-31 12:02:57 +0200

    Update Worrior.java

--- src/Worrior.java
+++ src/Worrior.java
@@ -1,12 +1,34 @@
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
 
-public class Worrior extends Units
-    {
-    public Worrior(ID id, Point point, Dimension dimension)
-        {
-        super(id, point, dimension);
-        }
+
+public class Warrior extends Units implements IClamp{
+
+    public Warrior(Point point, Dimension dimention, ID id, int x, int y) {
+        super(point, dimention, id);
+        this.setVelX(x);
+        this.setVelY(y);
     }
 
+    @Override
+    public void tick() {
+        this.getPoint().translate(this.getVelX(), this.getVelY());
+    }
 
+    @Override
+    public void paint(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
+    @Override
+    public float clamp(float var, float min, float max) {
+        if(var >= max)
+            return var = max;
+        else if(var <= min)
+            return var = min;
+        else 
+            return var;
+    }
+    
+}
