8ff32fac6e0c459aa1207cd1e963853157104169
hamaada29 <50968276+hamaada29@users.noreply.github.com>
2019-05-31 12:04:31 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,43 @@
-public class Main
-    {
-    public static void main(String[] args)
-        {
 
+    public class Main implements Runnable{
+
+    private boolean running;
+    private Thread thread;
+    private Handler handler;
+    
+    public void run(){
+        while(running){
+            
         }
     }
+    
+    public void tick(){
+        
+    }
+    
+    public void paint(){
+        
+    }
+    
+    public synchronized void start()
+    {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+    
+    public synchronized void stop()
+    {
+        try{
+        thread.join();
+        running = false;}
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+    }
+    
+}
