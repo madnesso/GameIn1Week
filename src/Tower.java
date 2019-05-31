0d6e611942c80e0923730c31dfd56fc27fabd02e
hamaada29 <50968276+hamaada29@users.noreply.github.com>
2019-05-31 12:01:18 +0200

    Update Tower.java

--- src/Tower.java
+++ src/Tower.java
@@ -1,52 +1,36 @@
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
 
-public class Tower extends GameObject
-    {
+public class Tower extends GameObject{
+    
+    private float health = 1000;
 
-    private int hight;
-    private int width;
-//    private int Health = 1000;
-    private Handler handler;
-
-    public Tower(ID id, Point point, Dimension dimension)
-        {
-        super(id, point, dimension);
-        }
-
-
-    @Override
-    public void tick()
-        {
-
-        if (Health <= 0)
-            {
-            handler.removeObject(this);
-
-            }
-        }
-
-    @Override
-    public void paint(Graphics g)
-        {
-
-        }
-
-    public int getCastleHealth()
-        {
-        return Health;
-        }
-
-    public void setCastleHealth(int castleHealth)
-        {
-        if (Health <= 1000)
-            {
-            Health = castleHealth;
-            }
-        else { Health = 1000; }
-        }
-
-    /*    public Rectangle getbounds(){
-            return new Rectangle(point.x,point.y,dimension.width,dimension.height);
-        }
-    */
+    public float getHealth() {
+        return health;
     }
+
+    public void setHealth(float health) {
+        if(health >= 1000)
+            this.health = 1000;
+        else if(health <= 0)
+            this.health = 0;
+        else 
+            this.health = health;
+    }
+
+    public Tower(Point point, Dimension dimention, ID id) {
+        super(point, dimention, id);
+    }
+
+    @Override
+    public void tick() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
