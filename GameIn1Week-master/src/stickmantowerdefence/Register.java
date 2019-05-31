ec30b639509f78aa8ebc23128f4dd5ac4dd4c531
omar2432 <50786739+omar2432@users.noreply.github.com>
2019-05-31 10:45:04 +0200

    Add files via upload

---
+++ GameIn1Week-master/src/stickmantowerdefence/Register.java
@@ -0,0 +1,4 @@
+package stickmantowerdefence;
+
+public class Register
+    {}
--- GameIn1Week-master/src/stickmantowerdefence/Shop.java
+++ GameIn1Week-master/src/stickmantowerdefence/Shop.java
@@ -1,68 +1,68 @@
 package stickmantowerdefence;
-//import User.java;
 public class Shop
     {
 
 private Handler hand;
+private User u;
 
-//private U
-
-public Shop(Handler h){
+public Shop(Handler h,User us){
     hand=h;
-
+u=us;
 }
-        public void upgradeMaxhealth(ID id){
+        public void upgradeMaxhealth(ID id) {
 
-            for (GameObject tempObject : hand.objects)
-            {
-                if (tempObject.getId().equals(id)){
-                    if(id.equals(ID.warrior)) {
-                        ((stickmantowerdefence.Warrior) tempObject).setMaxhealth(1000);
+            if (u.getgold()>5000) {
+
+                for (GameObject tempObject : hand.objects) {
+                    if (tempObject.getId().equals(id)) {
+                        if (id.equals(ID.warrior)) {
+                            ((stickmantowerdefence.Warrior) tempObject).setMaxhealth(1000);
+                        }
+
+                        if (id.equals(ID.archer)) {
+                            ((stickmantowerdefence.Archer) tempObject).setMaxhealth(600);
+                        }
+
+                        if (id.equals(ID.mage)) {
+                            ((stickmantowerdefence.Mage) tempObject).setMaxhealth(700);
+                        }
+
+                        if (id.equals(ID.healer)) {
+                            ((stickmantowerdefence.Healer) tempObject).setMaxhealth(800);
+                        }
+
+                        if (id.equals(ID.tower)) {
+                            ((stickmantowerdefence.Tower) tempObject).setMaxhealth(3000);
+                        }
+
+
                     }
-
-                    if(id.equals(ID.archer)) {
-                        ((stickmantowerdefence.Archer) tempObject).setMaxhealth(600);
-                    }
-
-                    if(id.equals(ID.mage)) {
-                        ((stickmantowerdefence.Mage) tempObject).setMaxhealth(700);
-                    }
-
-                    if(id.equals(ID.healer)) {
-                        ((stickmantowerdefence.Healer) tempObject).setMaxhealth(800);
-                    }
-
-                    if(id.equals(ID.tower)) {
-                        ((stickmantowerdefence.Tower) tempObject).setMaxhealth(3000);
-                    }
-
-
                 }
             }
-
             }
+
 
 
         public void upgradeattack(ID id){
 
-            for (GameObject tempObject : hand.objects)
-            {
-                if (tempObject.getId().equals(id)){
-                    if(id.equals(ID.warrior)) {
-                        ((Warrior) tempObject).attackinc= 200;
-                    }
+            if (u.getgold()>10000) {
+                for (GameObject tempObject : hand.objects) {
+                    if (tempObject.getId().equals(id)) {
+                        if (id.equals(ID.warrior)) {
+                            ((Warrior) tempObject).attackinc = 200;
+                        }
 
-                    if(id.equals(ID.archer)) {
-                        ((stickmantowerdefence.Archer) tempObject).attackinc= 100;
-                    }
+                        if (id.equals(ID.archer)) {
+                            ((stickmantowerdefence.Archer) tempObject).attackinc = 100;
+                        }
 
-                    if(id.equals(ID.mage)) {
-                        ((stickmantowerdefence.Mage) tempObject).attackinc= 100;
-                    }
+                        if (id.equals(ID.mage)) {
+                            ((stickmantowerdefence.Mage) tempObject).attackinc = 100;
+                        }
 
+                    }
                 }
             }
-
         }
 
 
